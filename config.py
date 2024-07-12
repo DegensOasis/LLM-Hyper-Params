f1ef257e79a3ed76f8288ae3406b1539c5fc7f88
DegensOasis <151899978+DegensOasis@users.noreply.github.com>
2024-07-12 03:05:45 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+MAX_TOKENS = 1000
+DEFAULT_PARAMS = {
+    'temperature': 1.0,
+    'top_p': 1.0,
+    'top_k': None,
+    'repetition_penalty': None,
+    'presence_penalty': 0.0,
+    'frequency_penalty': 0.0,
+    'max_tokens': MAX_TOKENS,
+}
